1b46193761714da788a8b5cf955333f9c25a52d4
Koen <penningmeester@badminton-pbo.be>
2022-12-05 22:56:57 +0100

    Initial setup

--- 2022/src/ex01.py
+++ 2022/src/ex01.py
@@ -1,11 +1,29 @@
 from AdventOfCode import AdventOfCode
 
-def part1(data):
+def part1(data: list):
+    calories = getCaloryList(data)
+    return sorted(calories,reverse=True)[0]
 
+def part2(data: list):
+    calories = getCaloryList(data)
+    topThree = sorted(calories,reverse=True)[0:3]
+    return sum(topThree)
 
-def part2(data):
-    pass
+def getCaloryList(data:list):
+    calories = []
+    currentCalories = 0
+    for calory in data:
+        if calory == "":
+            calories.append(currentCalories)
+            currentCalories = 0
+        else:
+            currentCalories += int(calory)
+    calories.append(currentCalories)
+    return calories
 
 
 ex = AdventOfCode(1)
 ex.executeTest(part1)
+ex.executeTest(part2)
+
+ex.execute(part1,part2)
