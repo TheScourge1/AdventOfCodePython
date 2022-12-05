8dc3bd4df14a6ff3c2ff0cfd92d59f0ef219e8b0
Koen <penningmeester@badminton-pbo.be>
2022-12-05 22:17:55 +0100

    Initial setup

---
+++ 2022/src/AdventOfCode.py
@@ -0,0 +1,37 @@
+import os
+from abc import ABC, abstractmethod
+
+class AdventOfCode:
+        _dataLocation = '../data/'
+        _inputFileName = 'input'
+        def __init__(self,exercise):
+            self.exercise = exercise
+            self.testData,self.data = self._loadData(exercise)
+
+        def _loadData(self, exercise):
+            data = [line.strip() for line in open(self._dataLocation + self.inputFileName(exercise)+'.txt','r').readlines()]
+            testData = []
+            for testFile in self._getTestFiles(exercise):
+                testData.append([line.strip() for line in open(self._dataLocation+testFile,'r').readlines()])
+            return testData,data
+
+        def _getTestFiles(self,exercise):
+            files = os.listdir(self._dataLocation)
+            return filter(lambda fileName: str(fileName).startswith(self.inputFileName(exercise)+'test'),files)
+
+        def inputFileName(self,exercise):
+            exerciseString = ('0' if exercise < 10 else '') + str(exercise)
+            return self._inputFileName + exerciseString
+
+        def execute(self,part1,part2):
+            print(f'Running exercise {self.exercise}\n')
+
+            print(f'solution part 1: {part1(self.data)}')
+            print(f'solution part 2: {part2(self.data)}')
+
+        def executeTest(self,part):
+            print(f'Testing exercise {self.exercise}\n')
+            for n in range(len(self.testData)):
+                print(f'test part {n+1}: {part(self.testData[n])}')
+
+ex = AdventOfCode(1)
---
+++ 2022/src/ex01.py
@@ -0,0 +1,11 @@
+from AdventOfCode import AdventOfCode
+
+def part1(data):
+
+
+def part2(data):
+    pass
+
+
+ex = AdventOfCode(1)
+ex.executeTest(part1)
