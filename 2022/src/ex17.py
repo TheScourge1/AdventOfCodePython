8118531738d727b867570c2fac1b8769a4e412ce
Koen <penningmeester@badminton-pbo.be>
2023-04-18 22:21:26 +0200

    Day 17 part 2

--- 2022/src/ex17.py
+++ 2022/src/ex17.py
@@ -4,12 +4,15 @@ from dataclasses import dataclass
 @dataclass
 class Canvas:
     view: list[list[int]]
+    last_block_dropped: list[int]
     height: int = 0
+    _canvas_size = 100000
 
     def __init__(self):
         self.view = []
+        self.last_block_dropped = [0] * self._canvas_size
         self.view.append([1] * 7)
-        for i in range(0,50000):
+        for i in range(0,self._canvas_size):
             self.view.append([0]*7)
 
 blocs = [[[1,1,1,1]],
@@ -22,15 +25,52 @@ blocs = [[[1,1,1,1]],
 def part1(data: list[str]):
     canvas = Canvas()
     index = 0
-    print(f"commands: {len(data[0])}")
+
     for i in range(0, 2022):
-        index = drop_bloc(canvas,blocs[i % 5], index, data[0])
+        index = drop_bloc(canvas, blocs[i % 5], index, data[0],i)
 
     #print_range(canvas.view[0:3200])
     return str(canvas.height)
 
 
-def drop_bloc(canvas: Canvas, next_bloc: list[list[int]], index: int, moves: str) -> int:
+def part2(data: list[str]):
+    canvas = Canvas()
+    index = 0
+
+    for i in range(0, 50000):
+        index = drop_bloc(canvas, blocs[i % 5], index, data[0],i+1)
+
+    string_canvas = "".join([str(conv_bits(row))+ "" for row in canvas.view])
+
+    for i in range(0, int(index/2)):
+        next_occurance = string_canvas[i+10:].find(string_canvas[i:i+10])+i+10
+        if next_occurance > 0 and string_canvas[i:next_occurance] == string_canvas[next_occurance:next_occurance+next_occurance-i]:
+            blocs_before_rep = canvas.last_block_dropped[i]
+            start_rep = i
+            rep_height = next_occurance - start_rep
+            blocs_in_rep = canvas.last_block_dropped[next_occurance] - blocs_before_rep
+            number_of_reps = int((1000000000000 - blocs_before_rep) / blocs_in_rep)
+            remaining_blocks = 1000000000000 - blocs_before_rep - blocs_in_rep * number_of_reps
+
+            remaining_height = 0
+            start_block = canvas.last_block_dropped[i]
+            for itt in range(i,next_occurance+1):
+                if canvas.last_block_dropped[itt]-start_block == remaining_blocks:
+                    remaining_height = itt-i
+
+            total_height = start_rep + rep_height*number_of_reps + remaining_height
+            return str(total_height)
+
+    return -1
+
+
+def conv_bits(input: list[int]) -> str:
+    res = input[0]
+    for i in input[1:]:
+        res = res*2+i
+    return chr(65+res)
+
+def drop_bloc(canvas: Canvas, next_bloc: list[list[int]], index: int, moves: str, block_index) -> int:
     h_offset = 2
     v_offset = canvas.height + 4
 
@@ -44,7 +84,7 @@ def drop_bloc(canvas: Canvas, next_bloc: list[list[int]], index: int, moves: str
             break
         v_offset -= 1
 
-    write_bloc(canvas, h_offset, v_offset, next_bloc)
+    write_bloc(canvas, h_offset, v_offset, next_bloc, block_index)
 
     return index
 
@@ -71,16 +111,14 @@ def block_overlapping(canvas: Canvas, h_offset: int, v_offset: int, bloc: list[l
                 return True
     return False
 
-def write_bloc(canvas: Canvas, h_offset: int, v_offset: int, bloc: list[list[int]]):
+def write_bloc(canvas: Canvas, h_offset: int, v_offset: int, bloc: list[list[int]],block_index: int):
     for i in range(0,len(bloc)):
+        canvas.last_block_dropped[i+v_offset] = block_index
         for j in range(0, len(bloc[0])):
             if bloc[i][j] == 1:
                 canvas.view[v_offset+i][j+h_offset] = 1
 
     canvas.height = max(v_offset + len(bloc)-1, canvas.height)
-
-def part2(data: list[str]):
-    pass
 
 
 def print_range(grid: list[list[int]]):
