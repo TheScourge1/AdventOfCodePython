10adea0d855f51ae359f3f08b209c178abdaeb9b
Koen <penningmeester@badminton-pbo.be>
2023-06-08 22:28:43 +0200

    Day 21 part 2

--- 2022/src/ex21.py
+++ 2022/src/ex21.py
@@ -5,15 +5,57 @@ from dataclasses import dataclass
 
 def part1(data:list[str]) -> str:
     commands = read_data(data)
-    for c in commands:
-        print(c + str(commands[c]))
-
     return str(get_value("root",commands))
 
 
 def part2(data:list[str]) -> str:
-    pass
+    commands = read_data(data)
+    root_command = commands["root"]
+    if contains_human(root_command[0],commands):
+        return str(get_equality_value(root_command[0],get_value(root_command[2],commands),commands))
+    else:
+        return str(get_equality_value(root_command[2], get_value(root_command[0],commands),commands))
 
+
+def get_equality_value(name: str, value: int, commands: dict[str, list[str]]) -> int:
+    command = commands[name]
+    if contains_human(command[2],commands):
+        left_value = get_value(command[0],commands)
+        match command[1]:
+            case "+":
+                equal_value = value - left_value
+            case "-":
+                equal_value = - (value - left_value)
+            case "*":
+                equal_value = int(value/left_value)
+            case "/":
+                equal_value = int(left_value / value)
+            case "_":
+                raise Exception("Unknown command" + command[1])
+        if command[2] == "humn":
+            return equal_value
+        else:
+            return get_equality_value(command[2],equal_value,commands)
+    elif contains_human(command[0], commands):
+        right_value = get_value(command[2],commands)
+        match command[1]:
+            case "+":
+                equal_value = value - right_value
+            case "-":
+                equal_value = value + right_value
+            case "*":
+                equal_value = int(value/right_value)
+            case "/":
+                equal_value = value * right_value
+            case "_":
+                raise Exception("Unknown command" + command[1])
+        if command[0] == "humn":
+            return equal_value
+        else:
+            return get_equality_value(command[0], equal_value, commands)
+
+    else:
+        raise Exception("humn command not found at: " + name)
 
 def get_value(name: str, commands: dict[str, list[str]]) -> int:
     command = commands[name]
@@ -36,6 +78,15 @@ def get_value(name: str, commands: dict[str, list[str]]) -> int:
         return result
 
 
+def contains_human(name: str, commands: dict[str, list[str]]) -> bool:
+    command = commands[name]
+    if len(command) == 1:
+        return name == "humn"
+    elif command[0] == "humn" or command[2] == "humn":
+        return True
+    else:
+        return contains_human(command[0],commands) or contains_human(command[2],commands)
+
 def read_data(data:list[str]) -> dict[str, list[str]]:
     result = {}
     for line in data:
@@ -49,6 +100,6 @@ def read_data(data:list[str]) -> dict[str, list[str]]:
 
 ex21 = AdventOfCode(21)
 ex21.executeTest(part1,"152")
-ex21.executeTest(part1,"301")
+ex21.executeTest(part2,"301")
 
 ex21.execute(part1,part2)
