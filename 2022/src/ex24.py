09aac4947f6bb27cec7be83d32ac0a0a9bf39f6f
Koen <penningmeester@badminton-pbo.be>
2023-10-03 21:28:19 +0200

    Day 24 part 1

---
+++ 2022/src/ex24.py
@@ -0,0 +1,89 @@
+from AdventOfCode import AdventOfCode
+import copy
+
+MAP_TO_INT = {'E': -2, '#': -1, '.': 0, '^': 1, '>': 10, 'v': 100, '<': 1000}
+MAP_TO_STR = {value: key for key, value in MAP_TO_INT.items()}
+BLIZZARD_MOVE = {1: (-1, 0), 10: (0, 1), 100: (1,0), 1000: (0, -1)}
+
+
+def part1(data: list[str]) -> str:
+    grid = read_grid(data)
+    grid[0][1]=-2
+    print_grid(grid)
+    print("")
+    return str(execute_round(grid,[(0,1)],1))
+
+
+def part2(data: list[str]) -> str:
+    pass
+
+
+def execute_round(grid: list[list[int]], possible_locations: list[tuple[int,int]], depth: int) -> int:
+    new_grid = evolve_grid(grid)
+    new_locations: set[tuple[int,int]] = {(0,1)}
+    #print(f"executing round: {depth} with {len(possible_locations)} options")
+    #print("")
+
+    for location in possible_locations:
+        for y_step in range(-1,2):
+            for x_step in range(-1,2):
+                new_loc = (location[0]+y_step,location[1]+x_step)
+                if new_loc[0] < 0 or (x_step != 0 and y_step != 0):
+                    continue
+                elif new_loc[0] == len(grid) -1 and new_loc[1] == len(grid[0]) - 2:
+                    return depth
+                elif new_grid[new_loc[0]][new_loc[1]] == 0:
+                    new_locations.add(new_loc)
+
+    return execute_round(new_grid,list(new_locations),depth+1)
+
+
+def evolve_grid(grid: list[list[int]]) -> list[list[int]]:
+    new_grid = copy.deepcopy(grid)
+    for i in range(1,len(new_grid)-1):
+        for j in range(1, len(new_grid[0]) - 1):
+            new_grid[i][j] = 0
+
+    for y in range(1,len(new_grid)-1):
+        for x in range(1, len(new_grid[0]) - 1):
+            if grid[y][x] > 0:
+                for move in get_moves(grid[y][x]):
+                    step = BLIZZARD_MOVE[move]
+                    new_y, new_x = y+step[0], x+step[1]
+
+                    if new_y == 0:
+                        new_y = len(new_grid)-2
+                    elif new_y == len(new_grid) - 1:
+                        new_y = 1
+                    if new_x == 0:
+                        new_x = len(new_grid[0]) - 2
+                    elif new_x == len(new_grid[0]) - 1:
+                        new_x = 1
+                    new_grid[new_y][new_x] += move
+
+    return new_grid
+
+
+def get_moves(combined_move: int) -> list[int]:
+    result = []
+    for i in range(4, -1, -1):
+        factor = 10 ** i
+        if combined_move // factor == 1:
+            result.append(factor)
+            combined_move = combined_move % factor
+    return result
+
+
+def read_grid(data: list[str]) -> list[list[int]]:
+    return [[MAP_TO_INT[s] for s in line.strip()] for line in data]
+
+
+def print_grid(grid: list[list[int]]):
+    for row in grid:
+        print("".join([MAP_TO_STR.get(r,"+") for r in row]))
+
+
+ex24 = AdventOfCode(24)
+ex24.executeTest(part1, "18")
+
+ex24.execute(part1, part2)
