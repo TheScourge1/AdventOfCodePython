f03132a649ddec0820793c1aa38b521142215e07
Koen <penningmeester@badminton-pbo.be>
2025-01-21 22:04:50 +0100

    2024 ex 7 part 2

--- 2024/src/ex07.py
+++ 2024/src/ex07.py
@@ -1,13 +1,15 @@
 from AdventOfCode import AdventOfCode
+import math
 
 
 def part1(data: list[str]):
     lines = read_data(data)
-    return sum([ln[0] for ln in lines if is_true(ln)])
+    return sum([ln[0] for ln in lines if is_true(ln,False)])
 
 
-def part2(data:list[str]):
-    pass
+def part2(data: list[str]):
+    lines = read_data(data)
+    return sum([ln[0] for ln in lines if is_true(ln,True)])
 
 
 def read_data(data: list[str]) -> list[(int, list[int])]:
@@ -20,13 +22,18 @@ def read_data(data: list[str]) -> list[(int, list[int])]:
     return result
 
 
-def is_true(line: (int, list[int])) -> bool:
+def is_true(line: (int, list[int]), part2: bool) -> bool:
+    if len(line[1]) == 1:
+        return line[0] == line[1][0]
+
     res_list = [line[1][0]]
     for nr in line[1][1:]:
         new_list = []
         for r in res_list:
-            new_list.append(r+nr)
-            new_list.append(r*nr)
+            new_vals = [r+nr,r*nr]
+            if part2:
+                new_vals.append(r*int(math.pow(10, int(math.log(nr, 10))+1))+nr)
+            new_list += [n for n in new_vals if n <= line[0]]
         res_list = new_list
     return res_list.count(line[0]) > 0
 
