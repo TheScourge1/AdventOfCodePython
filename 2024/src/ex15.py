aa30c69332afa91a7655ebad5548520a1adfc43f
Koen <penningmeester@badminton-pbo.be>
2025-03-05 20:55:05 +0100

    2024 ex 15 part 2

--- 2024/src/ex15.py
+++ 2024/src/ex15.py
@@ -6,20 +6,19 @@ MOVES = {"<": (0, -1), ">": (0, 1),"^": (-1, 0), "v": (1, 0)}
 
 def part1(data: list[str]):
     grid, commands = read_data(data)
-    start = find_start(grid)
-    location = start
-    #print_grid(grid)
-    #print("")
+    location = find_start(grid)
     for move in commands:
         location = execute_move(grid, location, move)
-        #print(f"move: {move}")
-        #print_grid(grid)
-        #print("")
     return get_gps_sum(grid)
 
 
 def part2(data: list[str]):
-    pass
+    grid, commands = read_data(data)
+    grid = duplicate_grid(grid)
+    location = find_start(grid)
+    for move in commands:
+        location = execute_move(grid, location, move)
+    return get_gps_sum(grid)
 
 
 def read_data(data: list[str]) -> (list[list[str]], str):
@@ -31,13 +30,25 @@ def read_data(data: list[str]) -> (list[list[str]], str):
             grid.append(list(line.strip()[0:len(line)-1]))
         elif re.match(command_pattern, line) is not None:
             commands += line.strip()
-
     return grid, commands
 
 
+def duplicate_grid(grid: list[list[str]]) -> list[list[str]]:
+    res = [list("#"*(len(grid[0])*2))]
+    mapping = {'#': ['#', '#'], '.': ['.', '.'], '@': ['@', '.'], 'O': ['[', ']']}
+
+    for i in range(1, len(grid)-1):
+        line = []
+        for j in range(0, len(grid[0])):
+            line += mapping[grid[i][j]]
+        res.append(line)
+    res.append(list("#"*(len(grid[0])*2)))
+    return res
+
+
 def find_start(grid: list[list[str]]) -> (int, int):
-    for i in range(0,len(grid)):
-        for j in range(0,len(grid[0])):
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[0])):
             if grid[i][j] == "@":
                 return i, j
     return None
@@ -45,27 +56,91 @@ def find_start(grid: list[list[str]]) -> (int, int):
 
 def execute_move(grid: list[list[str]], start: (int, int), move: str) -> (int, int):
     v = MOVES[move]
-    dist = 0
-    p = start
-    while grid[p[0]][p[1]] not in {"#", "."}:
-        dist += 1
-        p = p[0]+v[0], p[1]+v[1]
-    if grid[p[0]][p[1]] == ".":
-        while p != start:
-            new = p[0]-v[0], p[1]-v[1]
-            grid[p[0]][p[1]] = grid[new[0]][new[1]]
-            p = new
-        grid[p[0]][p[1]] = "."
-        return p[0]+v[0], p[1]+v[1]
+    if v[0] == 0:
+        return do_horizontal_move(grid, start, v[1])
+    else:
+        return do_vertical_move(grid, start, v[0])
+
+
+def do_horizontal_move(grid: list[list[str]], start: (int, int), v: int) -> (int,int):
+    y = start[0]
+    x = start[1]
+    while grid[y][x] not in {"#", "."}:
+        x += v
+    if grid[y][x] == ".":
+        while x != start[1]:
+            new_x = x - v
+            grid[y][x] = grid[y][new_x]
+            x = new_x
+        grid[y][x] = "."
+        return y, x+v
     else:
         return start
+
+
+def do_vertical_move(grid: list[list[str]], start: (int, int), v: int) -> (int, int):
+    x = start[1]
+    y = start[0]
+
+    if grid[y+v][x] == "#" or not can_move(grid, (y+v, x), v):
+        return start
+    elif grid[y+v][x] == "[" or grid[y+v][x] == "O":
+        move_box(grid,(y+v, x), v)
+    elif grid[y+v][x] == "]":
+        move_box(grid,(y+v,x-1), v)
+    elif grid[y+v][x] != ".":
+        raise Exception("Unexpected next value: "+grid[y+v][x])
+
+    grid[y+v][x] = "@"
+    grid[y][x] = "."
+    return y+v, x
+
+
+def move_box(grid: list[list[str]], start: (int, int), v: int):
+    y = start[0]
+    x = start[1]
+    if grid[y][x] not in {"[", "O", "."}:
+        raise Exception("Unexpected char found: " + grid[y][x])
+    if grid[y+v][x] == "#":
+        raise Exception("Unexpected block found: " + str((y+v,x)))
+    if grid[y+v][x] == "[" or grid[y+v][x] == "O":
+        move_box(grid, (y+v, x), v)
+    if grid[y+v][x] == "]":
+        move_box(grid, (y+v, x - 1), v)
+    if grid[y+v][x+1] == "[":
+        move_box(grid,(y+v, x+1), v)
+
+    if grid[y][x] == "[":
+        grid[y+v][x+1] = grid[y][x+1]
+        grid[y][x+1] = "."
+    grid[y+v][x] = grid[y][x]
+    grid[y][x] = "."
+
+    return
+
+
+def can_move(grid: list[list[str]], start: (int, int), v: int) -> bool:
+    y = start[0]
+    x = start[1]
+    if grid[y][x] == ".":
+        return True
+    elif grid[y][x] == "#":
+        return False
+    elif grid[y][x] == "[":
+        return can_move(grid,(y+v, x), v) and can_move(grid,(y+v, x+1), v)
+    elif grid[y][x] == "]":
+        return can_move(grid,(y+v, x), v) and can_move(grid,(y+v, x-1), v)
+    elif grid[y][x] == "O":
+        return can_move(grid,(y+v, x), v)
+    else:
+        raise Exception("unexpected char found : "+grid[y][x])
 
 
 def get_gps_sum(grid: list[list[str]]) -> int:
     result = 0
     for j in range(0, len(grid)):
         for i in range(0, len(grid[0])):
-            if grid[j][i] == "O":
+            if grid[j][i] == "O" or grid[j][i] == "[":
                 result += 100*j + i
     return result
 
@@ -77,5 +152,6 @@ def print_grid(grid: list[list[str]]):
 
 ex = AdventOfCode(15)
 ex.executeTest(part1, 10092)
+ex.executeTest(part2, 9021)
 
 ex.execute(part1, part2)
